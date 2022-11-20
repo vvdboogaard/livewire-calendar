a2dcb61c9ba869cd8eb777d3d24834f4139844c4
Vincent <5372790+vvdboogaard@users.noreply.github.com>
2022-11-21 00:05:17 +0100

    Add times to event

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -7,6 +7,11 @@
     <p class="text-sm font-medium">
         {{ $event['title'] }}
     </p>
+    <p class="mt-0 text-xs">
+        @if(isset($event['start_time']) && isset($event['end_time']))
+            {{$event['start_time']}} - {{$event['end_time']}}
+        @endif
+    </p>
     <p class="mt-2 text-xs">
         {{ $event['description'] ?? 'No description' }}
     </p>
